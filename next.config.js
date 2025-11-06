1f42106083d5be8bbe1539b33ec5fd473d2ffd5b
LetsGetRemixed <maveconstruction@gmail.com>
2025-11-05 20:37:02 -0600

    fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   // App Router is enabled by default in Next.js 13+
   outputFileTracingRoot: __dirname,
+  webpack: (config, { isServer }) => {
+    // Ensure path aliases are resolved correctly
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src'),
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
